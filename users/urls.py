1c3f9b641983d928c2996764afa269215cf1f25c
Colton Jackson <coltonajackson1@gmail.com>
2021-04-26 23:49:23 -0400

    Update - Remove Common Errors
    Removed common errors and bugs with the Django web application.

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, EditProfileView
 
 urlpatterns = [
 	path('signup/', SignUpView.as_view(), name='signup'),
+	path('<int:pk>/', EditProfileView.as_view(), name='edit_profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,19 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
-from .forms import CustomUserCreationForm
+from django.views.generic import CreateView, UpdateView
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 class SignUpView(CreateView):
 	form_class = CustomUserCreationForm
 	success_url = reverse_lazy('login')
 	template_name = 'signup.html'
+
+class EditProfileView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+	form_class = CustomUserChangeForm
+	success_url = reverse_lazy('home')
+	template_name = 'edit_profile.html'
+	login_url = 'login'
+
+	def test_func(self):
+		obj = self.get_object()
+		return obj.customuser == self.request.user
