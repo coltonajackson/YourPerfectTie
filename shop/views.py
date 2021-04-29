dc7505c7647c34eb3a03440d76ac0bff3a8aa7c5
Colton Jackson <53240735+coltonajackson@users.noreply.github.com>
2021-04-29 18:43:25 -0400

    Replenish with .gitignore

--- shop/views.py
+++
@@ -1,8 +0,0 @@
-#from django.shortcuts import render
-from django.views.generic import TemplateView
-
-'''def index(request):
-	return render(request, 'index.html')'''
-
-class HomePageView(TemplateView):
-	template_name = 'index.html'
