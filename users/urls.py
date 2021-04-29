7e004858eaf335855e11286d24755024fe8efbdc
Colton Jackson <53240735+coltonajackson@users.noreply.github.com>
2021-04-29 18:43:55 -0400

    Replenish with .gitignore

--- users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import SignUpView, EditProfileView
-
-urlpatterns = [
-	path('signup/', SignUpView.as_view(), name='signup'),
-	#path('<int:pk>/', EditProfileView.as_view(), name='edit_profile'),
-	path('edit_profile/', EditProfileView.as_view(), name='edit_profile'),
-]
